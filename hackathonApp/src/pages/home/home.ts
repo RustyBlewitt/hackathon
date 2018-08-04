8f8b268e7f660c89b52f0880f3bdb597ee97707d
Codie Little <codielittle@south-27-254.wireless.griffith.edu.au>
2018-08-04 14:27:54 +1000

    basic first screen

--- hackathonApp/src/pages/home/home.ts
+++ hackathonApp/src/pages/home/home.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, ModalController, NavParams, AlertController} from 'ionic-angular';
+import { Http } from '@angular/http';
+import 'rxjs/add/operator/map';
 
 @Component({
   selector: 'page-home',
@@ -10,5 +12,10 @@ export class HomePage {
   constructor(public navCtrl: NavController) {
 
   }
+  pullClasses(){
+    //this.http.get('https://www.reddit.com/r/gifs/new/.json?limit=10').map(res => res.json()).subscribe(data => {
+        //this.posts = data.data.children;
 
+    //});
+  }
 }

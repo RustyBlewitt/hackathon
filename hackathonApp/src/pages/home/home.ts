922f26e81518207db7c7d866094db3c59c8fab1c
Codie Little <codielittle@south-27-254.wireless.griffith.edu.au>
2018-08-04 16:41:14 +1000

    added course list

--- hackathonApp/src/pages/home/home.ts
+++ hackathonApp/src/pages/home/home.ts
@@ -13,16 +13,24 @@ import 'rxjs/add/operator/map';
 export class HomePage {
   courses: Observable<any>;
   course:any;
+  currentGPA:any;
+  targetGPA:any;
+  studentNumber:string;
   selectedCourse:any;
   constructor(public navCtrl: NavController, public httpClient: HttpClient) {
-    this.courses = this.httpClient.get('https://degrees.griffith.edu.au/rest-api/v3/Program/1534');
+    this.courses = this.httpClient.get('https://courseprofile.secure.griffith.edu.au/service/student-course-json.php?profileId=82444');
     this.courses.subscribe(data => {
       console.log('my data: ', data);
     })
   }
   pullClasses(){
-    this.selectedCourse = this.course;
-    console.log(this.selectedCourse);
-      this.navCtrl.push(CoursePage, this.selectedCourse);
+    if (this.currentGPA >= this.targetGPA || this.currentGPA > 7 || this.targetGPA > 7){
+        alert("Invalid information");
+    }
+    else{
+      this.navCtrl.push(CoursePage, {course: this.course, currentGPA: this.currentGPA, studentNumber: this.studentNumber});
+    }
+
+
   }
 }

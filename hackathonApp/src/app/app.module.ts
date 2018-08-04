fb32c6871195c780388958209c8fad858dd71e5a
Codie Little <codielittle@south-27-254.wireless.griffith.edu.au>
2018-08-04 15:33:43 +1000

    added new page

--- hackathonApp/src/app/app.module.ts
+++ hackathonApp/src/app/app.module.ts
@@ -3,23 +3,28 @@ import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
-
+import { HttpClientModule } from '@angular/common/http';
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { CoursePage } from '../pages/course/course';
+
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    HomePage,
+    CoursePage
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     IonicModule.forRoot(MyApp)
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    HomePage,
+    CoursePage
   ],
   providers: [
     StatusBar,
--- hackathonApp/src/pages/home/home.ts
+++ hackathonApp/src/pages/home/home.ts
@@ -1,14 +1,28 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, ModalController, NavParams, AlertController} from 'ionic-angular';
+import { Observable } from 'rxjs/Observable';
+import { HttpClient } from '@angular/common/http';
+import { CoursePage } from '../course/course';
+
+import 'rxjs/add/operator/map';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-
-  constructor(public navCtrl: NavController) {
-
+  courses: Observable<any>;
+  course:any;
+  selectedCourse:any;
+  constructor(public navCtrl: NavController, public httpClient: HttpClient) {
+    this.courses = this.httpClient.get('https://degrees.griffith.edu.au/rest-api/v3/Program/1534');
+    this.courses.subscribe(data => {
+      console.log('my data: ', data);
+    })
   }
-
+  pullClasses(){
+    this.selectedCourse = this.course;
+    console.log(this.selectedCourse);
+      this.navCtrl.push(CoursePage, this.selectedCourse);
+  }
 }
